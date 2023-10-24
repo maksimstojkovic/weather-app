00cec15dd017dd2a0eb144aca67a25c640ca200c
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-10-25 00:23:12 +1100

    Add page rendering and weather info update

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import "./style.scss";
+import { prependChild, appendChild } from "./helper";
+
+const renderPage = () => {
+  const body = document.querySelector("body");
+
+  const container = prependChild(body, "div", "container");
+  const content = appendChild(container, "div", "content");
+
+  appendChild(content, "p", "title").textContent = "Sydney Temperature";
+  const weather = appendChild(content, "div", "weather");
+  appendChild(weather, "div", "loader");
+  appendChild(weather, "div", "info");
+};
+
+const updateWeather = () => {
+  const loader = document.querySelector(".loader");
+  const info = document.querySelector(".info");
+
+  fetch(
+    "https://api.weatherapi.com/v1/current.json?q=Sydney&key=4726dd3ef25f4cf691f131949232410"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      loader.classList.add("hide");
+      // console.log(data);
+      info.textContent = data.current.temp_c + "°C";
+    });
+};
+
+renderPage();
+updateWeather();
